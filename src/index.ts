9515f8a35df4db57c1a334ee1e37bb4cc7891a99
Coffeemiller <dev2freedom@gmail.com>
2022-06-04 01:48:06 +0900

    Chain

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ interface BlockShape {
 }
 
 class Block implements BlockShape {
-    public hash: string;
+    public readonly hash: string;
     constructor(
-        public prevHash: string,
-        public height: number,
-        public data: string
+        public readonly prevHash: string,
+        public readonly height: number,
+        public readonly data: string
     ) {
         this.hash = Block.calculateHash(prevHash, height, data);
     }
@@ -19,4 +19,34 @@ class Block implements BlockShape {
         const toHash = `${prevHash}${height}${data}`;
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
- }
+}
+ 
+class Blockchain {
+    private blocks: Block[];
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if (this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data: string) {
+        const block = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(block);
+    }
+    public getBlocks() {
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+blockchain.getBlocks().push(new Block("xxxxxx", 111111, "HACKEDDDD"));
+// blockchain.getBlocks()[blockchain.getBlocks().length - 1].data = "hell 12312312312";
+
+console.log(blockchain.getBlocks());
